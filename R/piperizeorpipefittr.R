80e1176bb3ec0b853e67c1af5d09d5e8d9cabd6d
neerajt <neerajtandon25@gmail.com>
2016-05-07 11:52:17 -0500

    Adds pop function in `R/piperizeorpipefittr.R`

--- R/piperizeorpipefittr.R
+++ R/piperizeorpipefittr.R
@@ -4,6 +4,14 @@ test_str <- "bop_on(
     field_mouse ),
   head )"
 
+# a pop function that behaves like .pop() in
+# python or javascript
+pop <- function(vec){
+  newvec <- vec[-length(vec)]
+  vecname <- deparse(substitute(vec))
+  assign(vecname, newvec, envir = .GlobalEnv)
+  tail(vec, 1) 
+}
 
 piperize <- function(string){
   # remove whitespace
