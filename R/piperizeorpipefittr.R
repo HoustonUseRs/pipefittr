db9e82caab13f2009217fcbd98dca73cdec01174
neerajt <neerajtandon25@gmail.com>
2016-05-07 12:58:46 -0500

    Adds `make_output`, `make_list`, `pipefittr` funcs
    make_output works.
    make_list needs work.

--- R/piperizeorpipefittr.R
+++ R/piperizeorpipefittr.R
@@ -1,20 +1,59 @@
+require(dplyr)
+
 test_str <- "bop_on( 
   scoop_up( 
     hop_through(foo_foo, forest),
     field_mouse ),
   head )"
 
-# a pop function that behaves like .pop() in
-# python or javascript
-pop <- function(vec){
-  newvec <- vec[-length(vec)]
-  vecname <- deparse(substitute(vec))
-  assign(vecname, newvec, envir = .GlobalEnv)
-  tail(vec, 1) 
+# we're assuming we can get here ->
+test_list <- list(bop_on = c(".", "head"), 
+                  scoop_up = c(".", "field_mouse"), 
+                  hop_through = c(".", "forest"), 
+                  foo_foo = NULL)
+
+test_list2 <- list(summarise = c(".", "m = mean(wt)"), 
+                   filter = c(".", "mpg > 20"), 
+                   mtcars = NULL)
+
+make_output <- function(funclist) {
+  output = ""
+  
+  while(length(funclist) > 0){
+    # pop
+    func <- tail(funclist, 1)
+    funclist <- funclist[-length(funclist)]
+    funcname_str <- names(func)
+    
+    # unpack
+    if (length(func[[1]])) {
+      arg_str <- paste(func[[1]], collapse=", ")
+      func_str <- paste0(funcname_str, "(", arg_str, ")")
+    } else {
+      func_str <- funcname_str
+    }
+    
+    #add a %>%
+    if (length(funclist) == 0) {
+      full_str <- func_str
+    } else {
+      full_str <- paste(func_str, "%>%")
+    }
+    output <- paste(output, full_str)
+  }
+  output
 }
 
-piperize <- function(string){
+make_list <- function(string) {
   # remove whitespace
   string_ns <- gsub("\\s+", "", test_str)
   
+  # make list like test_list or test_list2
+  
+}
+
+pipefittr <- function(string) {
+  # l <- make_list(string)
+  l <- test_list
+  make_output(l)
 }
