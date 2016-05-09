8cf27d8f365aa04beeacf736de3a4044c2c69e58
J. Kim <jk@juyeongkim.com>
2016-05-09 00:22:17 -0500

    Reformat code, comment out hard coded variables, and remove "." on first arg

--- R/pipefittr.R
+++ R/pipefittr.R
@@ -1,30 +1,30 @@
-require(dplyr)
-
-test_str <- "bop_on( 
-  scoop_up( 
-    hop_through(foo_foo, forest),
-    field_mouse ),
-  head )"
-
-test_str2 <- "select(
-                filter(
-                  read.csv('data.csv', as.is=T)
-                )
-              )"
-# we're assuming we can get here ->
-test_list <- list(bop_on = c(".", "head"), 
-                  scoop_up = c(".", "field_mouse"), 
-                  hop_through = c(".", "forest"), 
-                  foo_foo = NULL)
-
-test_list2 <- list(summarise = c(".", "m = mean(wt)"), 
-                   filter = c(".", "mpg > 20"), 
-                   mtcars = NULL)
+# require(dplyr)
+# 
+# test_str <- "bop_on( 
+#   scoop_up( 
+#     hop_through(foo_foo, forest),
+#     field_mouse ),
+#   head )"
+# 
+# test_str2 <- "select(
+#                 filter(
+#                   read.csv('data.csv', as.is=T)
+#                 )
+#               )"
+# # we're assuming we can get here ->
+# test_list <- list(bop_on = c(".", "head"), 
+#                   scoop_up = c(".", "field_mouse"), 
+#                   hop_through = c(".", "forest"), 
+#                   foo_foo = NULL)
+# 
+# test_list2 <- list(summarise = c(".", "m = mean(wt)"), 
+#                    filter = c(".", "mpg > 20"), 
+#                    mtcars = NULL)
 
 make_output <- function(funclist) {
   output = ""
   
-  while(length(funclist) > 0){
+  while(length(funclist) > 0) {
     # pop
     func <- tail(funclist, 1)
     funclist <- funclist[-length(funclist)]
@@ -32,19 +32,22 @@ make_output <- function(funclist) {
     
     # unpack
     if (length(func[[1]])) {
-      arg_str <- paste(func[[1]], collapse=", ")
+      if(func[[1]][1] == ".") func[[1]] <- func[[1]][-1]
+      
+      arg_str <- paste(func[[1]], collapse = ", ")
       func_str <- paste0(funcname_str, "(", arg_str, ")")
     } else {
       func_str <- funcname_str
     }
     
-    #add a %>%
+    # add a %>%
     if (length(funclist) == 0) {
       full_str <- func_str
     } else {
-      full_str <- paste(func_str, "%>%")
+      full_str <- paste0(func_str, " %>% ")
     }
-    output <- paste(output, full_str)
+    
+    output <- paste0(output, full_str)
   }
   
   #remove leading whitespace
@@ -52,6 +55,7 @@ make_output <- function(funclist) {
   
   output
 }
+
 
 make_list <- function(string) {
   fwd <- string %>% 
